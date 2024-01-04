d625fc3bcadef40f0422013443f9ab6f04584ff0
renaheryani <153482845+renaheryani@users.noreply.github.com>
2024-01-04 23:16:48 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RentLogController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {  
+    return view('welcome');
+})->middleware('auth');
+
+Route::middleware('only_guest')->group(function(){
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register']);
+    Route::post('register', [AuthController::class, 'registerProcess']);
+});
+
+Route::middleware('auth')->group(function(){
+    Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware( 'only_admin');
+
+    Route::get('profile', [UserController::class, 'profile'])->middleware( 'only_client');
+
+//  BUKU
+    Route::get('books', [BookController::class, 'index']);
+    Route::get('book-add', [BookController::class, 'add']);
+    Route::post('book-add', [BookController::class, 'store']);
+
+
+//  KATEGORI
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('category-add', [CategoryController::class, 'add']);
+    Route::post('category-add', [CategoryController::class, 'store']);
+    Route::get('category-edit/{slug}', [CategoryController::class, 'edit']);
+    Route::put('category-edit/{slug}', [CategoryController::class, 'update']);
+    Route::get('category-delete/{slug}', [CategoryController::class, 'delete']);
+    Route::get('category-destroy/{slug}', [CategoryController::class, 'destroy']);
+
+    Route::get('users', [UserController::class, 'index']);
+
+    Route::get('rent-logs', [RentLogController::class, 'index']);
+});
